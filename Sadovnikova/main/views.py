d0cd466018359872869dd2337ef5edfdb7cafd15
Bogdanrg <70534359+Bogdanrg@users.noreply.github.com>
2023-03-16 08:25:08 +0300

    Home page

--- Sadovnikova/main/views.py
+++ Sadovnikova/main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 
 def home_page(request):
-    return render(request, template_name="main/layout.html")
+    return render(request, template_name="main/home.html", context={"title": "Home page"})
 
 
 def about_page(request):
-    return render(request, template_name="main/home.html")
+    return render(request, template_name="main/about.html", context={"title": "About me"})
